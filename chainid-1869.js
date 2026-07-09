da6a9446f394e9c65d098c65efbe8a7ba632380b
leeoxiang <leeoxiang@users.noreply.github.com>
2026-07-09 01:30:21 +0100

    Add Robinhood chain

---
+++ chainid-1869.js
@@ -0,0 +1,34 @@
+export const data = {
+  "name": "Robinhood",
+  "chain": "ROB",
+  "rpc": [
+    "https://rpc.robinhood.town"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Robinhood",
+    "symbol": "ROB",
+    "decimals": 18
+  },
+  "features": [
+    {
+      "name": "EIP155"
+    },
+    {
+      "name": "EIP1559"
+    }
+  ],
+  "infoURL": "https://www.robinhood.town",
+  "shortName": "robinhood",
+  "chainId": 1869,
+  "networkId": 1869,
+  "icon": "robinhood",
+  "explorers": [
+    {
+      "name": "Robinhood Explorer",
+      "url": "https://explorer.robinhood.town",
+      "icon": "robinhood",
+      "standard": "EIP3091"
+    }
+  ]
+}
